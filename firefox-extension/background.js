153ffc9a2ea57c5b73c54316f992203585b3de61
Sean <sean@albinodrought.com>
2019-11-12 00:16:00 -0800

    feat: initial PoC extension tag support

--- firefox-extension/background.js
+++ firefox-extension/background.js
@@ -1,25 +1,74 @@
 const importID = 'import-to-creamy-videos';
+const tagImportID = 'import-to-creamy-videos-tagged';
 
-browser.contextMenus.create({
-  id: importID,
-  title: 'Import to Creamy Videos',
-  contexts: ['all'],
-});
+let tagGroups = [];
+let tagSubmenus = {};
+
+function syncMenus() {
+  browser.contextMenus.removeAll();
+
+  tagGroups = [];
+  tagSubmenus = {};
+
+  browser.contextMenus.create({
+    id: importID,
+    title: 'Import to Creamy Videos',
+    contexts: ['all'],
+  });
+
+  browser.storage.sync.get('tagGroups').then((settingItem) => {
+    tagGroups = settingItem.tagGroups || [];
+
+    if (tagGroups.length > 0) {
+      browser.contextMenus.create({
+        id: tagImportID,
+        parentId: importID,
+        title: 'Default',
+        contexts: ['all'],
+      });
+    
+      tagGroups.forEach((submenu, i) => {
+        const id = `${tagImportID}-${i}`;
+        tagSubmenus[id] = i;
+        browser.contextMenus.create({
+          id,
+          parentId: importID,
+          title: submenu.text,
+          contexts: ['all'],
+        });
+      });
+    }
+  });
+}
+
+syncMenus();
+if (!browser.storage.onChanged.hasListener(syncMenus)) {
+  browser.storage.onChanged.addListener(syncMenus);
+}
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId !== importID) {
+  let tags;
+
+  if (info.menuItemId === importID || info.menuItemId === tagImportID) {
+    tags = [];
+  } else if (tagSubmenus[info.menuItemId] !== undefined) {
+    const tagGroup = tagGroups[tagSubmenus[info.menuItemId]];
+    tags = tagGroup.tags;
+  } else {
+    // unhandled menu item, ignore it
+    console.warn('unhandled menu item', info.menuItemId, info);
     return;
   }
 
   const url = info.linkUrl || tab.url;
-
+  
   browser.storage.sync.get('url').then((settingItem) => {
     fetch(settingItem.url || 'http://localhost:4000/', {
       method: 'post',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: 'url=' + encodeURIComponent(url),
+      body: 'url=' + encodeURIComponent(url) + '&tags=' + encodeURIComponent(tags.join(',')),
     }).then(function (resp) {
       if (!resp.ok) {
         throw new Error(resp.statusText);
--- firefox-extension/options.js
+++ firefox-extension/options.js
@@ -1,7 +1,24 @@
+function tagGroupsToText(tagGroups) {
+  return tagGroups.map((group) => {
+    return [group.text, ...group.tags].join(',');
+  }).join('\n');
+}
+
+function textToTagGroups(text) {
+  return text.split('\n').map((line) => {
+    const columns = line.split(',');
+    return {
+      text: columns[0],
+      tags: columns.slice(1),
+    };
+  }).filter(group => group.tags.length > 0);
+}
+
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
-    url: document.querySelector('#url').value
+    url: document.querySelector('#url').value,
+    tagGroups: textToTagGroups(document.querySelector('#tag-groups').value || ''),
   });
 
   var submitButton = document.querySelector('button[type="submit"]');
@@ -14,13 +31,14 @@ function saveOptions(e) {
 function restoreOptions() {
   function setCurrentChoice(result) {
     document.querySelector('#url').value = result.url || 'http://localhost:4000/';
+    document.querySelector('#tag-groups').value = tagGroupsToText(result.tagGroups);
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.sync.get('url');
+  var getting = browser.storage.sync.get(['url', 'tagGroups']);
   getting.then(setCurrentChoice, onError);
 }
 

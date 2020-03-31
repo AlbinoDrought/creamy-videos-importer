1dd4ca3d739746e5a0800f6f8adcac2600fd1683
Sean <sean@albinodrought.com>
2020-03-30 17:16:45 -0700

    feat(extension): add an "Ask" option to import with a one-time set of tags

--- firefox-extension/background.js
+++ firefox-extension/background.js
@@ -1,5 +1,6 @@
 const importID = 'import-to-creamy-videos';
 const tagImportID = 'import-to-creamy-videos-tagged';
+const promptImportID = 'import-to-creamy-videos-prompt';
 
 let tagGroups = [];
 let tagSubmenus = {};
@@ -16,28 +17,33 @@ function syncMenus() {
     contexts: ['all'],
   });
 
+  browser.contextMenus.create({
+    id: tagImportID,
+    parentId: importID,
+    title: 'Default',
+    contexts: ['all'],
+  });
+
+  browser.contextMenus.create({
+    id: promptImportID,
+    parentId: importID,
+    title: 'Ask',
+    contexts: ['all'],
+  });
+
   browser.storage.sync.get('tagGroups').then((settingItem) => {
     tagGroups = settingItem.tagGroups || [];
 
-    if (tagGroups.length > 0) {
+    tagGroups.forEach((submenu, i) => {
+      const id = `${tagImportID}-${i}`;
+      tagSubmenus[id] = i;
       browser.contextMenus.create({
-        id: tagImportID,
+        id,
         parentId: importID,
-        title: 'Default',
+        title: submenu.text,
         contexts: ['all'],
       });
-    
-      tagGroups.forEach((submenu, i) => {
-        const id = `${tagImportID}-${i}`;
-        tagSubmenus[id] = i;
-        browser.contextMenus.create({
-          id,
-          parentId: importID,
-          title: submenu.text,
-          contexts: ['all'],
-        });
-      });
-    }
+    });
   });
 }
 
@@ -48,9 +54,23 @@ if (!browser.storage.onChanged.hasListener(syncMenus)) {
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
   let tags;
+  let tagPromise = null;
 
   if (info.menuItemId === importID || info.menuItemId === tagImportID) {
     tags = [];
+  } else if (info.menuItemId === promptImportID) {
+    tagPromise = browser.tabs.executeScript(tab.id, {
+      code: `prompt('Enter tags, separated by comma:')`,
+    }).then((promptResult, err) => {
+      if (err) {
+        return [];
+      }
+
+      tags = promptResult[0] || '';
+      tags = tags.split(',');
+      tags = tags.map(tag => tag.trim());
+      return tags;
+    });
   } else if (tagSubmenus[info.menuItemId] !== undefined) {
     const tagGroup = tagGroups[tagSubmenus[info.menuItemId]];
     tags = tagGroup.tags;
@@ -60,33 +80,39 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
     return;
   }
 
-  const url = info.linkUrl || tab.url;
-  
-  browser.storage.sync.get('url').then((settingItem) => {
-    fetch(settingItem.url || 'http://localhost:4000/', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: 'url=' + encodeURIComponent(url) + '&tags=' + encodeURIComponent(tags.join(',')),
-    }).then(function (resp) {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.text();
-    }).then(function () {
-      browser.notifications.create({
-        'type': 'basic',
-        'title': 'Creamy Videos Importer',
-        'message': 'Import queued successully!',
+  if (!tagPromise) {
+    tagPromise = Promise.resolve(tags);
+  }
+
+  tagPromise.then((tags) => {
+    const url = info.linkUrl || tab.url;
+    
+    browser.storage.sync.get('url').then((settingItem) => {
+      fetch(settingItem.url || 'http://localhost:4000/', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'url=' + encodeURIComponent(url) + '&tags=' + encodeURIComponent(tags.join(',')),
+      }).then(function (resp) {
+        if (!resp.ok) {
+          throw new Error(resp.statusText);
+        }
+        return resp.text();
+      }).then(function () {
+        browser.notifications.create({
+          'type': 'basic',
+          'title': 'Creamy Videos Importer',
+          'message': 'Import queued successully!',
+        });
+      }).catch(function (ex) {
+        browser.notifications.create({
+          'type': 'basic',
+          'title': 'Creamy Videos Importer',
+          'message': 'Error importing video!\n' + ex.message,
+        });
+        throw ex;
       });
-    }).catch(function (ex) {
-      browser.notifications.create({
-        'type': 'basic',
-        'title': 'Creamy Videos Importer',
-        'message': 'Error importing video!\n' + ex.message,
-      });
-      throw ex;
     });
   });
 });
